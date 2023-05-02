a146844148bde28e48bf8bdf9f96345def3754f1
Alec Wixom <atwixom@gmail.com>
2023-05-02 13:45:20 -0600

    upload

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const { getMovies, addMovie, updateMovie, deleteMovie } = require('./controller.js')
+
+app.get('/api/movies', getMovies)
+app.post('/api/movies', addMovie)
+app.put('/api/movies/:id', updateMovie)
+app.delete('/api/movies/:id', deleteMovie)
+
+app.listen(4004, () => console.log('Binging Movies on port 4004'))
